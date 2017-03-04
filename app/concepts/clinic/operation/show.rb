a93041d6d0f43d61388b5c8b4009c020998ff122
Alex Coles <alex@alexbcoles.com>
2017-03-04 21:10:05 +0000

    [WIP] Presence check

--- app/concepts/clinic/operation/show.rb
+++ app/concepts/clinic/operation/show.rb
@@ -1,6 +1,7 @@
 class Clinic::Show < Trailblazer::Operation
   step Model(Clinic, :find)
   step Policy::Pundit(ClinicPolicy, :show?)
+  step ->(options) { options['present'] = true }
 
   extend Representer::DSL
   representer :serializer, V1::ClinicRepresenter
